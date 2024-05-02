ef736ac722134e9e0dbd7b0f8e7dd701cc507b65
Minto312 <karinto3219@gmail.com>
2024-05-02 04:34:08 +0000

    [add] plugins

---
+++ .config/nvim/lua/plugins/search.lua
@@ -0,0 +1,5 @@
+return {
+    "nvim-telescope/telescope.nvim",
+    "nvim-telescope/telescope-project.nvim",
+    "nvim-telescope/telescope-fzf-native.nvim",
+}
---
+++ .config/nvim/lua/plugins/ui.lua
@@ -0,0 +1,10 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    "akinsho/nvim-bufferline.lua",
+    "nvim-lualine/lualine.nvim",
+    "karb94/neoscroll.nvim",
+    "petertriho/nvim-scrollbar",
+    "folke/noice.nvim",
+    "kevinhwang91/nvim-ufo",
+
+}
