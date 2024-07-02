1d947d7a3fd68836d837b814e105729a7a0effd5
Minto312 <karinto3219@gmail.com>
2024-07-02 18:14:14 +0000

    [fix] modify plugin

--- .config/nvim/lua/plugins/mason-null-ls.lua
+++ .config/nvim/lua/plugins/mason-null-ls.lua
@@ -1,12 +1,8 @@
-{
-  'jay-babu/mason-null-ls.nvim',
-  event = { 'BufReadPre', 'BufNewFile' },
-  dependencies = {
-    'williamboman/mason.nvim',
-    'nvimtools/none-ls.nvim',
-  },
-+ opts = {
-+   handlers = {}
-+ },
-},
-
+return {
+    'jay-babu/mason-null-ls.nvim',
+    event = { 'BufReadPre', 'BufNewFile' },
+    dependencies = {
+        'williamboman/mason.nvim',
+        'nvimtools/none-ls.nvim',
+    }
+}
