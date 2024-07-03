59d49918390f66c86ebaeca9d5893e6942464251
Minto312 <karinto3219@gmail.com>
2024-07-03 06:50:15 +0000

    [add] git command alias

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,1 +1,12 @@
 alias ls='ls -lha'
+
+alias gs='git status'
+alias ga='git add'
+gc() {
+    combined_args=""
+    for arg in "$@"; do
+        combined_args="$combined_args $arg"
+    done
+
+    git commit -m "$combined_args"
+}
