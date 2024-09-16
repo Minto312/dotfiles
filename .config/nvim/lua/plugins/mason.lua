fbc4a248c78f8906da3f67052295079a49fb5327
Minto312 <karinto3219@gmail.com>
2024-09-16 13:09:05 +0900

    add ensure install

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,5 +1,16 @@
 return {
-    "williamboman/mason.nvim",
+    {
+        "williamboman/mason.nvim",
+        opts = {
+            ensure_installed = {
+            "stylua",
+            "shellcheck",
+            "shfmt",
+            "flake8",
+            "pyright"
+            },
+        },
+    },
     "williamboman/mason-lspconfig.nvim",
     "neovim/nvim-lspconfig",
 }
