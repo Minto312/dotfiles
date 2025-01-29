54b0a926f386bee0cd8e3896e947926da283e3a3
Minto312 <karinto3219@gmail.com>
2025-01-29 16:21:00 +0900

    add-wezterm: wslで起動するように

--- .config/wezterm/env.example.lua
+++ .config/wezterm/env.example.lua
@@ -1,5 +1,6 @@
 local env = {}
 
 env.background_image = "C:\\Users\\karinto\\Pictures\\壁紙\\wallpaperbetter.com_3840x2160.jpg"
+env.wsl_path = "C:\\Program Files\\WSL\\wsl.exe"
 
 return env
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,8 +1,13 @@
+local env = require("env")
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
+config.default_prog = { env.wsl_path, "--cd", "~" }
+
 config.font = wezterm.font("JetBrains Mono")
 config.color_scheme = "AdventureTime"
+config.default_cursor_style = "SteadyBar"
+
 config.background = require("background")
 
 return config
