b516b00c7d06c207b10cc2aee8ed7a582b391cf5
Minto312 <karinto3219@gmail.com>
2025-04-14 01:53:22 +0900

    feat: スクリプトへのパスを追加

--- zsh/environ.zsh
+++ zsh/environ.zsh
@@ -3,7 +3,8 @@ export LANG=C.UTF-8
 
 export SHELL="/usr/bin/zsh"
 
-path+=("$HOME/dotfiles/script")
+path+=("$HOME/dotfiles/scripts")
+
 export JAVA_HOME="$HOME/lib/jdk"
 path+=("$JAVA_HOME/bin")
 export ANDROID_HOME="$HOME/Android/Sdk"
