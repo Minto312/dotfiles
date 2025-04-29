0676743e429d6c5656786ae23f2f8da1bd4e2708
Minto312 <karinto3219@gmail.com>
2025-04-29 20:50:00 +0900

    feat: WezTermのコピー＆ペースト機能を追加

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,24 +2,14 @@ local env = require("env")
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
--- 同じディレクトリにあるすべてのLuaファイルを読み込む
-local function load_all_lua_files()
-    local files = wezterm.glob("*.lua")
-    for _, file in ipairs(files) do
-        if file ~= "wezterm.lua" then
-            require(file:gsub("%.lua$", ""))
-        end
-    end
-end
-
-load_all_lua_files()
-
 
 config.default_prog = { env.wsl_path, "--cd", "~" }
 
 config.font = wezterm.font("JetBrains Mono")
 config.color_scheme = "AdventureTime"
 config.default_cursor_style = "SteadyBar"
+config.keys = require("keys")
+
 
 config.background = require("background")
 
