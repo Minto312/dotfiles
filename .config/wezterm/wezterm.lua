37fea45d8dbee0e52a4b350ce6ee8d672a53b549
Minto312 <karinto3219@gmail.com>
2025-04-29 20:49:53 +0900

    feat: 同じディレクトリ内のすべてのLuaファイルを自動的に読み込む機能を追加

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,6 +2,19 @@ local env = require("env")
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
+-- 同じディレクトリにあるすべてのLuaファイルを読み込む
+local function load_all_lua_files()
+    local files = wezterm.glob("*.lua")
+    for _, file in ipairs(files) do
+        if file ~= "wezterm.lua" then
+            require(file:gsub("%.lua$", ""))
+        end
+    end
+end
+
+load_all_lua_files()
+
+
 config.default_prog = { env.wsl_path, "--cd", "~" }
 
 config.font = wezterm.font("JetBrains Mono")
