38c583c78b01778cde3b4b4404077371822ec18c
Minto312 <karinto3219@gmail.com>
2025-10-03 13:38:17 +0900

    feat: mmdcの変更

--- scripts/mmdc.sh
+++ scripts/mmdc.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/zsh
-# usage: mmdc.sh <filename.mmd>
+# usage: mmdc.sh <mmd text>
 
 if [ -z "$1" ]; then
-    echo "Usage: mmdc.sh <filename.mmd>"
+    echo "Usage: mmdc.sh <mmd text>"
     exit 1
 fi
 
-docker run --rm -u `id -u`:`id -g` -v $PWD:/data minlag/mermaid-cli -i $1 -o $1.png
+filename=temp
+
+echo "$1" > $filename.mmd
+
+if [ -n "$2" ]; then
+    docker run --rm -u `id -u`:`id -g` -v $PWD:/data minlag/mermaid-cli -i $filename.mmd -o $filename.$2
+fi
+
+docker run --rm -u `id -u`:`id -g` -v $PWD:/data minlag/mermaid-cli -i $filename.mmd -o $filename.png
