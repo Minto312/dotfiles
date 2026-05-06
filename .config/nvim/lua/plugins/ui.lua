9ef0375145f18ff5d6900989d65baa5be20f6726
Minto312 <karinto3219@gmail.com>
2026-05-06 09:45:51 +0000

    fix(nvim): スムーズスクロールを無効化
    neoscroll.nvim を削除し、snacks.nvim の scroll も無効化

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -2,8 +2,8 @@ return {
     "nvim-treesitter/nvim-treesitter",
     "akinsho/bufferline.nvim",
     "nvim-lualine/lualine.nvim",
-    "karb94/neoscroll.nvim",
     "petertriho/nvim-scrollbar",
     "folke/noice.nvim",
     { "kevinhwang91/nvim-ufo", dependencies = { "kevinhwang91/promise-async" } },
+    { "snacks.nvim", opts = { scroll = { enabled = false } } },
 }
