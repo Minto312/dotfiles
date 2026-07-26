75d4b09b5ae77cd8dbfd113010ccd3ed74bf1d04
Minto312 <karinto3219@gmail.com>
2026-07-26 16:54:40 +0000

    refactor(nvim): neo-tree/search プラグイン設定を削除し snacks.explorer に寄せる
    snacks.picker の explorer で ignored = true を有効化し、gitignore 済みファイルも起動時のツリーに出す。

--- .config/nvim/lua/plugins/search.lua
+++
@@ -1,5 +0,0 @@
-return {
-    "nvim-telescope/telescope.nvim",
-    "nvim-telescope/telescope-project.nvim",
-    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-}
--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -5,5 +5,14 @@ return {
     "petertriho/nvim-scrollbar",
     "folke/noice.nvim",
     { "kevinhwang91/nvim-ufo", dependencies = { "kevinhwang91/promise-async" } },
-    { "snacks.nvim", opts = { scroll = { enabled = false } } },
+    { "snacks.nvim", opts = {
+        scroll = { enabled = false },
+        picker = {
+            sources = {
+                -- 起動時の snacks.explorer で gitignore 済みファイルも表示する
+                -- (dotfile も出したい場合は hidden = true も追加)
+                explorer = { ignored = true },
+            },
+        },
+    } },
 }
